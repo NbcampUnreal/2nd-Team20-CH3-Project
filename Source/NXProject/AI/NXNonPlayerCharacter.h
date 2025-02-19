bcb2aa5283fc0473b1b734f64ce87ec994cf29ff
KJH <kim2005kgy@gmail.com>
2025-02-19 21:53:12 +0900

    재추가

---
+++ Source/NXProject/AI/NXNonPlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Player/NXCharacterBase.h"
+#include "NXNonPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NXPROJECT_API ANXNonPlayerCharacter : public ANXCharacterBase
+{
+	GENERATED_BODY()
+	
+};
