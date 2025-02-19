6a73a2fba2232e2d14de6ca95ed0b5323a739413
KJH <kim2005kgy@gmail.com>
2025-02-19 21:10:31 +0900

    프로젝트 파일 추가

---
+++ Source/NXProject/Game/NXGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NXGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NXPROJECT_API ANXGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
