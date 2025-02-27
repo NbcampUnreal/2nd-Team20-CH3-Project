5d961521ef2584573b890d9c989df35574ad3149
LAPTOP-9ENSSA8V\krnam <krnams2000@gmail.com>
2025-02-27 17:13:46 +0900

    UI 작업3

--- Source/NXProject/UI/NXUIPlayerController_Loading.cpp
+++ Source/NXProject/UI/NXUIPlayerController_Loading.cpp
@@ -8,21 +8,10 @@ void ANXUIPlayerController_Loading::BeginPlay()
 {
 	Super::BeginPlay();
 
-    AGameModeBase* GameMode = UGameplayStatics::GetGameMode(this);
-    if (IsValid(GameMode))
-    {
-        // "NextLevel=" 옵션에서 레벨 이름 가져오기
-        FString NextLevelString = UGameplayStatics::ParseOption(GameMode->OptionsString, TEXT("NextLevel"));
-
-        // 값이 유효하면 해당 레벨로 이동
-        if (!NextLevelString.IsEmpty())
-        {
-            UE_LOG(LogTemp, Log, TEXT("Next Level: %s"), *NextLevelString);
-            UGameplayStatics::OpenLevel(this, *NextLevelString);
-        }
-        else
-        {
-            UE_LOG(LogTemp, Warning, TEXT("NextLevel option not found!"));
-        }
-    }
+	AGameModeBase* GameMode = UGameplayStatics::GetGameMode(this);
+	if (IsValid(GameMode) == true)
+	{
+		FString NextLevelString = UGameplayStatics::ParseOption(GameMode->OptionsString, FString(TEXT("NextLevel")));
+		UGameplayStatics::OpenLevel(GameMode, *NextLevelString, false);
+	}
 }
