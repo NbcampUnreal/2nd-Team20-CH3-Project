07019813eb2dcc803e88953f2a4d4450521ff563
LAPTOP-9ENSSA8V\krnam <krnams2000@gmail.com>
2025-02-28 18:06:46 +0900

    힐링아이템 오버랩, 회복 로직 코드 추가

--- Source/NXProject/Gimmick/NXHealingItem.cpp
+++ Source/NXProject/Gimmick/NXHealingItem.cpp
@@ -1,4 +1,6 @@
 #include "Gimmick/NXHealingItem.h"
+#include "Player/NXPlayerCharacter.h"
+#include "Engine/Engine.h" 
 
 ANXHealingItem::ANXHealingItem()
 {
@@ -8,10 +10,21 @@ ANXHealingItem::ANXHealingItem()
 
 void ANXHealingItem::ActivateItem(AActor* Activator)
 {
-    if (Activator && Activator->ActorHasTag("Player"))
+    ANXPlayerCharacter* PlayerCharacter = Cast<ANXPlayerCharacter>(Activator);
+    if (PlayerCharacter)
     {
-        GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Green, FString::Printf(TEXT("Player gained %d HP!"), HealAmount));
+        // 현재 체력 + 회복량, 최대 체력을 넘지 않도록 처리
+        float CurrentHealth = PlayerCharacter->GetCurrentHealth();
+        float MaxHealth = PlayerCharacter->GetMaxHealth();
+        float NewHealth = FMath::Min(CurrentHealth + HealAmount, MaxHealth);
 
+        PlayerCharacter->SetHealth(NewHealth);
+
+        // 디버그 메시지 또는 UI 알림
+        GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Green,
+            FString::Printf(TEXT("Healed for %d HP"), HealAmount));
+
+        // 아이템 제거
         DestroyItem();
     }
 }
